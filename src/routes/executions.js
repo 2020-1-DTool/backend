39684208c230f97f603ac4f9adbe127d1b41a3d1
alexteixeira <alex.teixeira@edu.pucrs.br>
2020-05-21 22:58:17 -0300

    Atualização de requisições Feita (Erro Foreign Key Pendente)

--- src/routes/executions.js
+++ src/routes/executions.js
@@ -4,31 +4,43 @@ import { Auth, ExecutionService } from "../services"
 import config from "../config"
 
 export default appRouter => {
-  const router = Router() // /execute
+  const router = Router()
   const authService = Container.get(Auth)
 
-  
+  //Validação do Codigo de Entrada
   router.post("/", authService.middlewares.requireAppAdministration, (req, res) => {
+    const executionService = Container.get(ExecutionService)
+    const deviceToken = req.body.deviceToken
 
-    res.json({
-      auth: req.auth,
-      perms: Auth.PERMISSIONS,
-    })
-  })
-  
-  const deviceToken = req.body.deviceToken
-
-  if (deviceToken == null){
+  // Confere se o Token for vazio e se não tiver nenhuma execução
+    if (deviceToken == null || req.body.executions.length <= 0) {
       res.status(400).json({
         "code": "bad_request/missing_fields",
         "message": "fields 'deviceToken' and 'executions' are required, and there should be at least one execution"
       })
-  }
-  /*const executeRoleID
-  const executeActivityID
-  const executeTimestamp
-  const executeDuration
-  const executeDeviceToken*/
+      return
+    }
 
+    for (var item of req.body.executions) {
+
+      const activityId = item.activityId
+      const roleId = item.roleId
+      const timestamp = item.timestamp
+      const duration = item.duration  
+
+  // Confere se os valores não são nulos e se tiverem dados faltando
+      if (activityId == null || roleId == null || timestamp == null || duration == null) {
+        res.status(400).json({
+          "code": "bad_request/missing_fields",
+          "message": "fields 'deviceToken' and 'executions' are required, and there should be at least one execution"
+        })
+        return
+      }
+    };
+
+  // Se passar do for, esta tudo certo com os dados e o payload tem uma ou mais execuções a serem enviadas ao banco
+    executionService.createExecution(req.body)
+    res.status(200)
+  })
   appRouter.use("/executions", router)
 }
--- src/services/ExecutionService.js
+++ src/services/ExecutionService.js
@@ -2,20 +2,17 @@ import { Op } from "sequelize"
 import { ExecutionDAO } from "../models"
 
 export default class ExecutionService {
-  /**
-   * Retorna a permissão de um código de instituição e as informações da instituição
-   * relacionada.
-   *
-   * Se o código não fizer referência a nenhuma instituição conhecida, retorn `null`.
-   *
-   * @param {string} accessCode Código de acesso de uma instituição.
-   * @returns {{ permission: string, institution: { name: string }}} Objeto com
-   * permissão do código e informações da instituição. Se o código não corresponder
-   * a nenhuma instituição, retorna `null`.
-   */
 
-    async createExecution(array){
-
-    
-    }
+  async createExecution(payload) {
+    try {
+    payload.executions.forEach(function(itm){
+      itm.deviceToken = payload.deviceToken;
+     });
+      await ExecutionDAO.bulkCreate(payload.executions)
+      
+    } catch (error) {
+      console.log(error); 
+    } 
+    throw new Error('Ocorreu um problema com a inserção de dados no banco de dados');
+  }
 }
