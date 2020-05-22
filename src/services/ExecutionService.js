57ce2c40c8c80aa63e3a1eabf2e426db3601d288
alexteixeira <alex.teixeira@edu.pucrs.br>
2020-05-22 00:24:48 -0300

    Atualização de requisições Feita (Erro Foreign Key Arrumado)

--- src/services/ExecutionService.js
+++ src/services/ExecutionService.js
@@ -4,15 +4,9 @@ import { ExecutionDAO } from "../models"
 export default class ExecutionService {
 
   async createExecution(payload) {
-    try {
     payload.executions.forEach(function(itm){
       itm.deviceToken = payload.deviceToken;
      });
       await ExecutionDAO.bulkCreate(payload.executions)
-      
-    } catch (error) {
-      console.log(error); 
-    } 
-    throw new Error('Ocorreu um problema com a inserção de dados no banco de dados');
   }
 }
