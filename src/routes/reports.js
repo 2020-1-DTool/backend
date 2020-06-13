9ddb93f995335190989225c576fb239e77fc8fc5
Lucas <lucascordeirocastro@gmail.com>
2020-06-12 21:42:45 -0300

    Alterando 'professional' para 'role' e filtrando resultados por role

--- src/routes/reports.js
+++ src/routes/reports.js
@@ -9,34 +9,36 @@ export default appRouter => {
   // relatório simples (para gráficos do app)
   router.get("/simple", authService.middlewares.authenticated, async (req, res) => {
 
-    if (req.body.technology == null || req.body.professional == null) {
+    if (req.body.technology == null || req.body.role == null) {
       res.status(400).json({
         code: "bad_request/missing_fields",
-        message: "Either 'technology' and 'professional' values are required.",
+        message: "Either 'technology' and 'role' values are required.",
       })
       return
     }
 
-    if (typeof req.body.technology !== 'number' || typeof req.body.professional !== 'number') {
+    if (typeof req.body.technology !== 'number' || typeof req.body.role !== 'number') {
       res.status(400).json({
         code: "bad_request/missing_fields",
-        message: "Either 'technology' and 'professional' values should be numbers.",
+        message: "Either 'technology' and 'role' values should be numbers.",
       })
       return
     }
 
     const executionService = Container.get(ExecutionService)
     const reportSimple = await executionService.exportConsolidatedExecutions(req.body.technology)
+
+    const reportSimpleFilteredByRole = reportSimple.filter(rep => rep.roleID === req.body.role)
     
-    if (Object.keys(reportSimple).length === 0) {
+    if (Object.keys(reportSimpleFilteredByRole).length === 0) {
       res.status(404).json({
         code: "not_found/no_data_found",
-        message: "There is no metrics related to this technology ID",
+        message: "There is no metrics related to this technology and role",
       })
       return
     }
 
-    res.status(200).json(reportSimple)
+    res.status(200).json(reportSimpleFilteredByRole)
   })
 
   // relatório completo (para gestores, XLSX)
