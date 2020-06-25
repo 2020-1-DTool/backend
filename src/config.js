a83f46730504da76ee5e2bb4dffe6927fc94554a
Rafael Victor Ruwer Araujo <rafaelruweraraujo@gmail.com>
2020-06-25 16:46:22 -0300

    Implement create health institution route

--- src/config.js
+++ src/config.js
@@ -5,5 +5,8 @@ export default {
   jwtSecret: process.env.JWT_SECRET ?? "secret-key",
   jwtExpiresIn: process.env.JWT_EXPIRES_IN ?? "1h",
   reportsCronFrequency: process.env.REPORTS_CRON_FREQUENCY ?? "*/15 * * * *",
+
+  mailUsername: process.env.MAIL_USERNAME,
+  mailPassword: process.env.MAIL_PASSWORD,
   adminCode: process.env.ADMIN_CODE ?? "0000",
 }
